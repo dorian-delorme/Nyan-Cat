a6bd53325c58fd31327dcd28e2b71e9cf78c1b86
Dorian Delorme <dorian.delorme@hetic.net>
2017-10-14 23:29:18 +0200

    Add static mode

--- script/main.js
+++ script/main.js
@@ -16,6 +16,10 @@ canvas.addEventListener ( 'mousemove',function(e)
 	mouse.y = e.clientY;
 });
 
+var iter = false;
+var ref = {};
+var refTail = {};
+
 // =============== STARCREATION =============== //
 
 var coords = [];
@@ -66,8 +70,8 @@ var tails = [];
 function create_tail()
 {
 	var tail     = {};
-	tail.x       = mouse.x;
-	tail.y       = mouse.y;
+	tail.x       = refTail.x;
+	tail.y       = refTail.y;
 	tail.width   = 10;
 	tails.push( tail );
 
@@ -124,6 +128,10 @@ function draw()
 		}
 	}
 
+	window.addEventListener('click', function() {
+		iter = !iter;
+	})
+
 function nyan()
 	{
 		// ==============    NYANCAT    ================= //
@@ -132,182 +140,182 @@ function nyan()
 	
 		context.beginPath();
 		context.fillStyle = '#ffcc99';
-		context.fillRect(mouse.x-10,mouse.y-55,150,120);
+		context.fillRect(ref.x-10,ref.y-55,150,120);
 
 		//top
 
 		context.fillStyle = '#ff99ff';
-		context.fillRect(mouse.x,mouse.y-45,130,100);
+		context.fillRect(ref.x,ref.y-45,130,100);
 
 
 		//border
 
 		context.fillStyle = 'black';
-		context.fillRect(mouse.x-20,mouse.y-45,10,110);
-		context.fillRect(mouse.x-10,mouse.y-55,10,10);
-		context.fillRect(mouse.x,mouse.y-65,130,10);
-		context.fillRect(mouse.x+130,mouse.y-55,10,10);
-		context.fillRect(mouse.x-10,mouse.y+55,10,10);
-		context.fillRect(mouse.x,mouse.y+65,140,10);;
-		context.fillRect(mouse.x+140,mouse.y-45,10,110);
-		context.fillRect(mouse.x-10,mouse.y+65,10,10);
+		context.fillRect(ref.x-20,ref.y-45,10,110);
+		context.fillRect(ref.x-10,ref.y-55,10,10);
+		context.fillRect(ref.x,ref.y-65,130,10);
+		context.fillRect(ref.x+130,ref.y-55,10,10);
+		context.fillRect(ref.x-10,ref.y+55,10,10);
+		context.fillRect(ref.x,ref.y+65,140,10);;
+		context.fillRect(ref.x+140,ref.y-45,10,110);
+		context.fillRect(ref.x-10,ref.y+65,10,10);
 
 		//marmelade
 
 		context.fillStyle = '#ff3399';
-		context.fillRect(mouse.x+15,mouse.y-25,10,10);
-		context.fillRect(mouse.x+55,mouse.y-25,10,10);
-		context.fillRect(mouse.x+85,mouse.y-35,10,10);
-		context.fillRect(mouse.x+45,mouse.y,10,10);
-		context.fillRect(mouse.x+20,mouse.y+35,10,10);
-		context.fillRect(mouse.x+55,mouse.y+25,10,10);
-		context.fillRect(mouse.x+10,mouse.y+15,10,10);
-		context.fillRect(mouse.x+100,mouse.y-15,10,10);
-		context.fillRect(mouse.x+30,mouse.y,10,10);
-		context.fillRect(mouse.x+40,mouse.y-40,10,10);
-		context.fillRect(mouse.x+45,mouse.y+40,10,10);
+		context.fillRect(ref.x+15,ref.y-25,10,10);
+		context.fillRect(ref.x+55,ref.y-25,10,10);
+		context.fillRect(ref.x+85,ref.y-35,10,10);
+		context.fillRect(ref.x+45,ref.y,10,10);
+		context.fillRect(ref.x+20,ref.y+35,10,10);
+		context.fillRect(ref.x+55,ref.y+25,10,10);
+		context.fillRect(ref.x+10,ref.y+15,10,10);
+		context.fillRect(ref.x+100,ref.y-15,10,10);
+		context.fillRect(ref.x+30,ref.y,10,10);
+		context.fillRect(ref.x+40,ref.y-40,10,10);
+		context.fillRect(ref.x+45,ref.y+40,10,10);
 
 		//eraser_topleft
 
 		context.fillStyle = '#ffcc99';
-		context.fillRect(mouse.x,mouse.y-45,10,10);
-		context.fillRect(mouse.x,mouse.y-35,10,10);
-		context.fillRect(mouse.x+10,mouse.y-45,10,10);
+		context.fillRect(ref.x,ref.y-45,10,10);
+		context.fillRect(ref.x,ref.y-35,10,10);
+		context.fillRect(ref.x+10,ref.y-45,10,10);
 	
 		//eraser_bottomleft
 
-		context.fillRect(mouse.x,mouse.y+35,10,10);
-		context.fillRect(mouse.x+10,mouse.y+45,10,10);
-		context.fillRect(mouse.x,mouse.y+45,10,10);
+		context.fillRect(ref.x,ref.y+35,10,10);
+		context.fillRect(ref.x+10,ref.y+45,10,10);
+		context.fillRect(ref.x,ref.y+45,10,10);
 	
 		//eraser_topright
 
-		context.fillRect(mouse.x+120,mouse.y-45,10,10);
-		context.fillRect(mouse.x+120,mouse.y-35,10,10);
-		context.fillRect(mouse.x+110,mouse.y-45,10,10);
+		context.fillRect(ref.x+120,ref.y-45,10,10);
+		context.fillRect(ref.x+120,ref.y-35,10,10);
+		context.fillRect(ref.x+110,ref.y-45,10,10);
 	
 		//PAW FORWARD RIGHT
 
 		context.fillStyle = '#a6a6a6';
-		context.fillRect(mouse.x+80,mouse.y+75,20,10);
+		context.fillRect(ref.x+80,ref.y+75,20,10);
 	
 		context.fillStyle = 'black';
-		context.fillRect(mouse.x+80,mouse.y+85,20,10);
-		context.fillRect(mouse.x+70,mouse.y+75,10,20);
-		context.fillRect(mouse.x+100,mouse.y+75,10,10);
+		context.fillRect(ref.x+80,ref.y+85,20,10);
+		context.fillRect(ref.x+70,ref.y+75,10,20);
+		context.fillRect(ref.x+100,ref.y+75,10,10);
 	
 		// PAW FORWARD LEFT 
 
 		context.fillStyle = '#a6a6a6';
-		context.fillRect(mouse.x+130,mouse.y+75,20,10);
-		context.fillRect(mouse.x+140,mouse.y+65,10,10);
+		context.fillRect(ref.x+130,ref.y+75,20,10);
+		context.fillRect(ref.x+140,ref.y+65,10,10);
 	
 		context.fillStyle = 'black';
-		context.fillRect(mouse.x+130,mouse.y+85,20,10);
-		context.fillRect(mouse.x+120,mouse.y+75,10,10);
-		context.fillRect(mouse.x+150,mouse.y+65,10,30);
+		context.fillRect(ref.x+130,ref.y+85,20,10);
+		context.fillRect(ref.x+120,ref.y+75,10,10);
+		context.fillRect(ref.x+150,ref.y+65,10,30);
 	
 		// PAW BACK LEFT
 
 		context.fillStyle = '#a6a6a6';
-		context.fillRect(mouse.x+20,mouse.y+75,20,10);
+		context.fillRect(ref.x+20,ref.y+75,20,10);
 	
 		context.fillStyle = 'black';
-		context.fillRect(mouse.x+20,mouse.y+85,20,10);
-		context.fillRect(mouse.x+10,mouse.y+75,10,20);
-		context.fillRect(mouse.x+40,mouse.y+75,10,10);
+		context.fillRect(ref.x+20,ref.y+85,20,10);
+		context.fillRect(ref.x+10,ref.y+75,10,20);
+		context.fillRect(ref.x+40,ref.y+75,10,10);
 	
 		// PAW BACK RIGHT
 
 		context.fillStyle = '#a6a6a6';
-		context.fillRect(mouse.x-40,mouse.y+65,30,10);
-		context.fillRect(mouse.x-30,mouse.y+55,10,10);
-		context.fillRect(mouse.x-40,mouse.y+75,20,10);
+		context.fillRect(ref.x-40,ref.y+65,30,10);
+		context.fillRect(ref.x-30,ref.y+55,10,10);
+		context.fillRect(ref.x-40,ref.y+75,20,10);
 	
 		context.fillStyle = 'black';
-		context.fillRect(mouse.x-50,mouse.y+65,10,20);
-		context.fillRect(mouse.x-50,mouse.y+85,30,10);
-		context.fillRect(mouse.x-20,mouse.y+75,10,10);
-		context.fillRect(mouse.x-40,mouse.y+55,10,10);
-		context.fillRect(mouse.x-30,mouse.y+45,10,10);
+		context.fillRect(ref.x-50,ref.y+65,10,20);
+		context.fillRect(ref.x-50,ref.y+85,30,10);
+		context.fillRect(ref.x-20,ref.y+75,10,10);
+		context.fillRect(ref.x-40,ref.y+55,10,10);
+		context.fillRect(ref.x-30,ref.y+45,10,10);
 	
 		//TAIL
 
 		context.fillStyle = '#a6a6a6';
-		context.fillRect(mouse.x-40,mouse.y-5,20,20);
-		context.fillRect(mouse.x-60,mouse.y-5,20,10);
-		context.fillRect(mouse.x-70,mouse.y-25,20,20);
+		context.fillRect(ref.x-40,ref.y-5,20,20);
+		context.fillRect(ref.x-60,ref.y-5,20,10);
+		context.fillRect(ref.x-70,ref.y-25,20,20);
 	
 		context.fillStyle = 'black';
-		context.fillRect(mouse.x-40,mouse.y+15,20,10);
-		context.fillRect(mouse.x-50,mouse.y-15,30,10);
-		context.fillRect(mouse.x-60,mouse.y+5,20,10);
-		context.fillRect(mouse.x-70,mouse.y-5,10,10);
-		context.fillRect(mouse.x-80,mouse.y-25,10,20);
-		context.fillRect(mouse.x-70,mouse.y-35,20,10);
-		context.fillRect(mouse.x-50,mouse.y-25,10,10);
+		context.fillRect(ref.x-40,ref.y+15,20,10);
+		context.fillRect(ref.x-50,ref.y-15,30,10);
+		context.fillRect(ref.x-60,ref.y+5,20,10);
+		context.fillRect(ref.x-70,ref.y-5,10,10);
+		context.fillRect(ref.x-80,ref.y-25,10,20);
+		context.fillRect(ref.x-70,ref.y-35,20,10);
+		context.fillRect(ref.x-50,ref.y-25,10,10);
 	
 
 		// HEAD STROKE
 
-		context.fillRect(mouse.x+100,mouse.y-25,40,10);
-		context.fillRect(mouse.x+90,mouse.y-35,10,10);
-		context.fillRect(mouse.x+140,mouse.y-35,10,10);
-		context.fillRect(mouse.x+150,mouse.y-45,10,10);
-		context.fillRect(mouse.x+160,mouse.y-55,20,10);
-		context.fillRect(mouse.x+180,mouse.y-45,10,30);
-		context.fillRect(mouse.x+190,mouse.y-15,10,50);
-		context.fillRect(mouse.x+180,mouse.y+35,10,10);
-		context.fillRect(mouse.x+170,mouse.y+45,10,10);
-		context.fillRect(mouse.x+70,mouse.y+55,100,10);
-		context.fillRect(mouse.x+80,mouse.y-45,10,10);
-		context.fillRect(mouse.x+60,mouse.y-55,20,10);
-		context.fillRect(mouse.x+50,mouse.y-45,10,30);
-		context.fillRect(mouse.x+40,mouse.y-15,10,50);
-		context.fillRect(mouse.x+50,mouse.y+35,10,10);
-		context.fillRect(mouse.x+60,mouse.y+45,10,10);
+		context.fillRect(ref.x+100,ref.y-25,40,10);
+		context.fillRect(ref.x+90,ref.y-35,10,10);
+		context.fillRect(ref.x+140,ref.y-35,10,10);
+		context.fillRect(ref.x+150,ref.y-45,10,10);
+		context.fillRect(ref.x+160,ref.y-55,20,10);
+		context.fillRect(ref.x+180,ref.y-45,10,30);
+		context.fillRect(ref.x+190,ref.y-15,10,50);
+		context.fillRect(ref.x+180,ref.y+35,10,10);
+		context.fillRect(ref.x+170,ref.y+45,10,10);
+		context.fillRect(ref.x+70,ref.y+55,100,10);
+		context.fillRect(ref.x+80,ref.y-45,10,10);
+		context.fillRect(ref.x+60,ref.y-55,20,10);
+		context.fillRect(ref.x+50,ref.y-45,10,30);
+		context.fillRect(ref.x+40,ref.y-15,10,50);
+		context.fillRect(ref.x+50,ref.y+35,10,10);
+		context.fillRect(ref.x+60,ref.y+45,10,10);
 	
 
 		// HEAD FILL
 
 		context.fillStyle = '#999999';
-		context.fillRect(mouse.x+70,mouse.y-5,100,60);
-		context.fillRect(mouse.x+50,mouse.y-5,20,40);
-		context.fillRect(mouse.x+170,mouse.y-5,20,40);
-		context.fillRect(mouse.x+60,mouse.y-35,20,30);
-		context.fillRect(mouse.x+160,mouse.y-35,20,30);
-		context.fillRect(mouse.x+150,mouse.y-25,10,20);
-		context.fillRect(mouse.x+80,mouse.y-25,10,20);
-		context.fillRect(mouse.x+90,mouse.y-15,10,10);
-		context.fillRect(mouse.x+140,mouse.y-15,10,10);
-		context.fillRect(mouse.x+60,mouse.y+35,120,10);
-		context.fillRect(mouse.x+100,mouse.y-15,40,10);
-		context.fillRect(mouse.x+150,mouse.y-35,10,20);
-		context.fillRect(mouse.x+160,mouse.y-45,20,10);
-		context.fillRect(mouse.x+60,mouse.y-45,20,10);
-		context.fillRect(mouse.x+80,mouse.y-35,10,10);
-		context.fillRect(mouse.x+90,mouse.y-25,10,10);
-		context.fillRect(mouse.x+50,mouse.y-15,140,10);
-		context.fillRect(mouse.x+140,mouse.y-25,10,10);
+		context.fillRect(ref.x+70,ref.y-5,100,60);
+		context.fillRect(ref.x+50,ref.y-5,20,40);
+		context.fillRect(ref.x+170,ref.y-5,20,40);
+		context.fillRect(ref.x+60,ref.y-35,20,30);
+		context.fillRect(ref.x+160,ref.y-35,20,30);
+		context.fillRect(ref.x+150,ref.y-25,10,20);
+		context.fillRect(ref.x+80,ref.y-25,10,20);
+		context.fillRect(ref.x+90,ref.y-15,10,10);
+		context.fillRect(ref.x+140,ref.y-15,10,10);
+		context.fillRect(ref.x+60,ref.y+35,120,10);
+		context.fillRect(ref.x+100,ref.y-15,40,10);
+		context.fillRect(ref.x+150,ref.y-35,10,20);
+		context.fillRect(ref.x+160,ref.y-45,20,10);
+		context.fillRect(ref.x+60,ref.y-45,20,10);
+		context.fillRect(ref.x+80,ref.y-35,10,10);
+		context.fillRect(ref.x+90,ref.y-25,10,10);
+		context.fillRect(ref.x+50,ref.y-15,140,10);
+		context.fillRect(ref.x+140,ref.y-25,10,10);
 		
 		// HEAD CONTENT
 
 		context.fillStyle = 'black';
-		context.fillRect(mouse.x+150,mouse.y-5,20,20);
-		context.fillRect(mouse.x+80,mouse.y-5,20,20);
-		context.fillRect(mouse.x+130,mouse.y+5,10,10);
-		context.fillRect(mouse.x+90,mouse.y+35,70,10);
-		context.fillRect(mouse.x+90,mouse.y+25,10,20);
-		context.fillRect(mouse.x+150,mouse.y+25,10,20);
-		context.fillRect(mouse.x+120,mouse.y+25,10,20);
+		context.fillRect(ref.x+150,ref.y-5,20,20);
+		context.fillRect(ref.x+80,ref.y-5,20,20);
+		context.fillRect(ref.x+130,ref.y+5,10,10);
+		context.fillRect(ref.x+90,ref.y+35,70,10);
+		context.fillRect(ref.x+90,ref.y+25,10,20);
+		context.fillRect(ref.x+150,ref.y+25,10,20);
+		context.fillRect(ref.x+120,ref.y+25,10,20);
 	
 		context.fillStyle = '#FFFFFF';
-		context.fillRect(mouse.x+150,mouse.y-5,10,10); 	
-		context.fillRect(mouse.x+80,mouse.y-5,10,10);
+		context.fillRect(ref.x+150,ref.y-5,10,10); 	
+		context.fillRect(ref.x+80,ref.y-5,10,10);
 	
 		context.fillStyle = '#ff9999';
-		context.fillRect(mouse.x+170,mouse.y+15,20,20);
-		context.fillRect(mouse.x+60,mouse.y+15,20,20);
+		context.fillRect(ref.x+170,ref.y+15,20,20);
+		context.fillRect(ref.x+60,ref.y+15,20,20);
 
 }
 		
@@ -342,6 +350,18 @@ function loop()
 	text();
 	remove_tail();
 	delete_star();
+
+	if (iter) {
+		ref.x = mouse.x;
+		ref.y = mouse.y;
+		refTail.x = mouse.x;
+		refTail.y = mouse.y;
+	} else {
+		ref.x = context.canvas.width / 2;
+		ref.y = context.canvas.height / 2;
+		refTail.x = context.canvas.width / 2;
+		refTail.y = context.canvas.height / 2;
+	}
 }
 requestAnimationFrame(loop);
 
